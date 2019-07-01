8dc81392187cb90ccc25d891a8f17752970764b4
vikaslaghata <vikasla.ghata@gmail.com>
2019-07-01 14:04:30 +0200

    Python classes, graphs using external module matplotlib,

--- java/src/com/spring/framework/TestClass.java
+++ java/src/com/spring/framework/TestClass.java
@@ -2,24 +2,12 @@ package com.spring.framework;
 
 import com.spring.framework.bean.StudentBean;
 import com.spring.framework.conf.AppConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestClass {
 
-    @Autowired
-    private StudentBean bean;
-
-
-
-    private void printStudentName(){
-        System.out.println(bean.getName()+" and "+bean.getType());
-    }
-
     public static void main(String args[]){
-       // TestClass testClass = new TestClass();
-      //  testClass.printStudentName();
 
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         StudentBean bean = context.getBean("studentBean", StudentBean.class);
